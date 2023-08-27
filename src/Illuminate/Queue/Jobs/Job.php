6332c17916c2d3850aab7220cbc3bdb29fef101c
Aleksandr Fisun <texnikru@gmail.com>
2023-08-27 14:30:47 +1000

    + Job's tracking with Sentry

--- src/Illuminate/Queue/Jobs/Job.php
+++ src/Illuminate/Queue/Jobs/Job.php
@@ -4,98 +4,100 @@ use DateTime;
 
 abstract class Job {
 
-	/**
-	 * The job handler instance.
-	 *
-	 * @var mixed
-	 */
-	protected $instance;
+    /**
+     * The job handler instance.
+     *
+     * @var mixed
+     */
+    protected $instance;
 
-	/**
-	 * The IoC container instance.
-	 *
-	 * @var \Illuminate\Container\Container
-	 */
-	protected $container;
+    /**
+     * The IoC container instance.
+     *
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
 
-	/**
-	 * The name of the queue the job belongs to.
-	 *
-	 * @var string
-	 */
-	protected $queue;
+    /**
+     * The name of the queue the job belongs to.
+     *
+     * @var string
+     */
+    protected $queue;
 
-	/**
-	 * Indicates if the job has been deleted.
-	 *
-	 * @var bool
-	 */
-	protected $deleted = false;
+    /**
+     * Indicates if the job has been deleted.
+     *
+     * @var bool
+     */
+    protected $deleted = false;
 
-	/**
-	 * Fire the job.
-	 *
-	 * @return void
-	 */
-	abstract public function fire();
+    /**
+     * Fire the job.
+     *
+     * @return void
+     */
+    abstract public function fire();
 
-	/**
-	 * Delete the job from the queue.
-	 *
-	 * @return void
-	 */
-	public function delete()
-	{
-		$this->deleted = true;
-	}
-
-	/**
-	 * Determine if the job has been deleted.
-	 *
-	 * @return bool
-	 */
-	public function isDeleted()
-	{
-		return $this->deleted;
-	}
-
-	/**
-	 * Release the job back into the queue.
-	 *
-	 * @param  int   $delay
-	 * @return void
-	 */
-	abstract public function release($delay = 0);
-
-	/**
-	 * Get the number of times the job has been attempted.
-	 *
-	 * @return int
-	 */
-	abstract public function attempts();
-
-	/**
-	 * Get the raw body string for the job.
-	 *
-	 * @return string
-	 */
-	abstract public function getRawBody();
-
-	/**
-	 * Resolve and fire the job handler method.
-	 *
-	 * @param  array  $payload
-	 * @return void
-	 */
-	protected function resolveAndFire(array $payload)
+    /**
+     * Delete the job from the queue.
+     *
+     * @return void
+     */
+    public function delete()
     {
-        if (!isset($payload['job'])) {
+        $this->deleted = true;
+    }
+
+    /**
+     * Determine if the job has been deleted.
+     *
+     * @return bool
+     */
+    public function isDeleted()
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * Release the job back into the queue.
+     *
+     * @param  int   $delay
+     * @return void
+     */
+    abstract public function release($delay = 0);
+
+    /**
+     * Get the number of times the job has been attempted.
+     *
+     * @return int
+     */
+    abstract public function attempts();
+
+    /**
+     * Get the raw body string for the job.
+     *
+     * @return string
+     */
+    abstract public function getRawBody();
+
+    /**
+     * Resolve and fire the job handler method.
+     *
+     * @param  array  $payload
+     * @return void
+     */
+    protected function resolveAndFire(array $payload)
+    {
+        if( ! isset($payload['job'])) {
             return;
         }
 
-        if (class_exists('Tideways\Profiler')) {
-            \Tideways\Profiler::start();
-            \Tideways\Profiler::setTransactionName($payload['job']);
+        if(class_exists('\Sentry\SentrySdk')) {
+            $checkInId = \Sentry\captureCheckIn(
+                $monitorSlug = $payload['job'],
+                \Sentry\CheckInStatus::inProgress()
+            );
         }
 
         try {
@@ -103,100 +105,120 @@ abstract class Job {
             $this->instance = $this->resolve($class);
             $this->instance->{$method}($this, $payload['data']);
 
-        } catch (\Exception $e) {
-            if (class_exists('Tideways\Profiler')) {
-                \Tideways\Profiler::logException($e);
-            }
-        } catch (\Throwable $e) {
-            if (class_exists('Tideways\Profiler')) {
-                \Tideways\Profiler::logException($e);
+            if(class_exists('\Sentry\SentrySdk')) {
+                \Sentry\captureCheckIn(
+                    $monitorSlug,
+                    \Sentry\CheckInStatus::ok(),
+                    NULL,
+                    NULL,
+                    $checkInId
+                );
             }
         }
+        catch(\Exception $e) {
+            if(class_exists('\Sentry\SentrySdk')) {
+                \Sentry\captureCheckIn(
+                    $monitorSlug,
+                    \Sentry\CheckInStatus::error(),
+                    NULL,
+                    NULL,
+                    $checkInId
+                );
+            }
+        }
+        catch(\Throwable $e) {
+            if(class_exists('\Sentry\SentrySdk')) {
+                \Sentry\captureCheckIn(
+                    $monitorSlug,
+                    \Sentry\CheckInStatus::error(),
+                    NULL,
+                    NULL,
+                    $checkInId
+                );
+            }
 
-        if (class_exists('Tideways\Profiler')) {
-            \Tideways\Profiler::stop();
         }
     }
 
-	/**
-	 * Resolve the given job handler.
-	 *
-	 * @param  string  $class
-	 * @return mixed
-	 */
-	protected function resolve($class)
-	{
-		return $this->container->make($class);
-	}
+    /**
+     * Resolve the given job handler.
+     *
+     * @param  string  $class
+     * @return mixed
+     */
+    protected function resolve($class)
+    {
+        return $this->container->make($class);
+    }
 
-	/**
-	 * Parse the job declaration into class and method.
-	 *
-	 * @param  string  $job
-	 * @return array
-	 */
-	protected function parseJob($job)
-	{
-		$segments = explode('@', $job);
+    /**
+     * Parse the job declaration into class and method.
+     *
+     * @param  string  $job
+     * @return array
+     */
+    protected function parseJob($job)
+    {
+        $segments = explode('@', $job);
 
-		return count($segments) > 1 ? $segments : array($segments[0], 'fire');
-	}
+        return count($segments) > 1 ? $segments : array($segments[0], 'fire');
+    }
 
-	/**
-	 * Determine if job should be auto-deleted.
-	 *
-	 * @return bool
-	 */
-	public function autoDelete()
-	{
-		return isset($this->instance->delete);
-	}
+    /**
+     * Determine if job should be auto-deleted.
+     *
+     * @return bool
+     */
+    public function autoDelete()
+    {
+        return isset($this->instance->delete);
+    }
 
-	/**
-	 * Calculate the number of seconds with the given delay.
-	 *
-	 * @param  \DateTime|int  $delay
-	 * @return int
-	 */
-	protected function getSeconds($delay)
-	{
-		if ($delay instanceof DateTime)
-		{
-			return max(0, $delay->getTimestamp() - $this->getTime());
-		}
+    /**
+     * Calculate the number of seconds with the given delay.
+     *
+     * @param  \DateTime|int  $delay
+     * @return int
+     */
+    protected function getSeconds($delay)
+    {
+        if ($delay instanceof DateTime)
+        {
+            return max(0, $delay->getTimestamp() - $this->getTime());
+        }
 
-		return (int) $delay;
-	}
+        return (int) $delay;
+    }
 
-	/**
-	 * Get the current system time.
-	 *
-	 * @return int
-	 */
-	protected function getTime()
-	{
-		return time();
-	}
+    /**
+     * Get the current system time.
+     *
+     * @return int
+     */
+    protected function getTime()
+    {
+        return time();
+    }
 
-	/**
-	 * Get the name of the queued job class.
-	 *
-	 * @return string
-	 */
+    /**
+     * Get the name of the queued job class.
+     *
+     * @return string
+     */
     public function getName()
     {
         $decoded = json_decode($this->getRawBody(), true);
         return isset($decoded['job']) ? $decoded['job'] : '(undefined)';
     }
 
-	/**
-	 * Get the name of the queue the job belongs to.
-	 *
-	 * @return string
-	 */
-	public function getQueue()
-	{
-		return $this->queue;
-	}
+    /**
+     * Get the name of the queue the job belongs to.
+     *
+     * @return string
+     */
+    public function getQueue()
+    {
+        return $this->queue;
+    }
 
 }
